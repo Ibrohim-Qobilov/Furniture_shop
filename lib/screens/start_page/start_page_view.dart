ed5fb1e0c69e12673b0eac03c73f461646f9c50d
ibrokhim-kobilov <ibrokhim112@gmail.com>
2021-12-19 12:36:47 +0500

    start page ended

--- lib/screens/start_page/start_page_view.dart
+++ lib/screens/start_page/start_page_view.dart
@@ -2,6 +2,7 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
 import 'package:furniture_shop/core/components/size_config.dart';
+import 'package:furniture_shop/core/constants/color.dart';
 import './start_page_view_model.dart';
 
 class StartPageView extends StartPageViewModel {
@@ -21,19 +22,36 @@ class StartPageView extends StartPageViewModel {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            makeyour(),
-            homeFunc(),
-            longText(),
+            Padding(
+              padding: EdgeInsets.only(
+                  left: getUniqueWidth(40), top: getUniqueHeight(210)),
+              child: textFunk(
+                  "Make Your", ConstColor.greyColor60, 18.0, "Gelasio",FontWeight.normal),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  left: getUniqueWidth(40), top: getUniqueHeight(20.0)),
+              child: textFunk(
+                  "HOME BEAUTIFUL", ConstColor.blackColor30, 24.0, "Galasio",FontWeight.normal),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  left: getUniqueWidth(60), top: getUniqueHeight(30.0)),
+              child: textFunk(
+                  "The best simple place where you discover most wonderful furnitures\nand make your home beautiful",
+                  ConstColor.blackColor30,
+                  18.0,
+                  "Galasio",FontWeight.w300),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: getUniqueHeight(180), left: getUniqueWidth(120)),
+              child: ElevatedButton(
+                  onPressed: () {}, child: const Text('Get Started')),
+            ),
           ],
         ),
       ),
     );
   }
-
-  
-  }
-
-  
-
-  
-
+}
--- lib/screens/start_page/start_page_view_model.dart
+++ lib/screens/start_page/start_page_view_model.dart
@@ -1,56 +1,19 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:furniture_shop/core/components/size_config.dart';
+import 'package:furniture_shop/core/constants/color.dart';
 import './start_page.dart';
 
 abstract class StartPageViewModel extends State<StartPage> {
-  Padding makeyour() {
-    return Padding(
-      padding:
-          EdgeInsets.only(left: getUniqueWidth(30), top: getUniqueHeight(231)),
-      child: DefaultTextStyle(
-        style:
-            TextStyle(fontSize: 24, color: Colors.grey, fontFamily: "Gelasio"),
-        child: AnimatedTextKit(
-          animatedTexts: [
-            TypewriterAnimatedText(
-              'Make your',
-              speed: const Duration(microseconds: 30000),
-            ),
-          ],
-          totalRepeatCount: 1,
-          displayFullTextOnTap: true,
-          stopPauseOnTap: true,
-        ),
-      ),
+  textFunk(text, color, size, fontfamily, fontWeight) {
+    return Text(
+      text,
+      style: TextStyle(
+          fontFamily: fontfamily,
+          fontSize: size,
+          color: color,
+          fontWeight: fontWeight),
     );
   }
-
-  Padding homeFunc() {
-    return Padding(
-      padding:
-          EdgeInsets.only(left: getUniqueWidth(30), top: getUniqueHeight(15.0)),
-      child: const Text(
-        'HOME BEAUTIFUL',
-        style:
-            TextStyle(fontSize: 24, color: Colors.black, fontFamily: "Gelasio"),
-      ),
-    );
-  }
-
-  Padding longText() {
-    return Padding(
-      padding: EdgeInsets.only(
-          left: getUniqueWidth(100), top: getUniqueHeight(30.0)),
-      child: const Text(
-        'The best simple place where you discover most wonderful furnitures and make your home beautiful',
-        style: TextStyle(
-            fontFamily: 'Gelasio', color: Color(0xff808080), fontSize: 18),
-      ),
-    );
-    // Add your state and logic here
-  }
-  textFunk(text, color, ){
-    return Text(text);
-  }
+  
 }
